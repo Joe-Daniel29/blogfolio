1b8f2a919600a116feaf5bd8116410f4c30eb175
Joe-Daniel29 <very.joe.daniel@gmail.com>
2025-04-20 23:32:17 +0530

    import

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 import json
 import os
 
