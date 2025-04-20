5799ac5187f788a746817307909c9864c20d7e53
Joe-Daniel29 <very.joe.daniel@gmail.com>
2025-04-20 22:14:40 +0530

    added domains

--- src/pages/BlogPostDetail.jsx
+++ src/pages/BlogPostDetail.jsx
@@ -10,7 +10,7 @@ const BlogPostDetail = () => {
   useEffect(() => {
     const fetchPost = async () => {
       try {
-        const response = await fetch(`http://127.0.0.1:8000/posts/${token}`);
+        const response = await fetch(`https://api.joe.sputh.me/posts/${token}`);
         if (!response.ok) {
           throw new Error("Post not found");
         }
